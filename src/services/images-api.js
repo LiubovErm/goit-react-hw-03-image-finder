3beaa4afaf206b5926b4c69966b9dc35a03650f0
LiubovErm <ermakovaluba27@gmail.com>
2022-10-20 17:12:23 +0300

    api

---
+++ src/services/images-api.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_KEY = '29948630-157933f0e62faad2f834f63f1';
+ 
+export const fetchImages = async (query, page) => {
+  const { response } = await axios.get(
+    `https://pixabay.com/api/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  );
+  return response;
+};
+
